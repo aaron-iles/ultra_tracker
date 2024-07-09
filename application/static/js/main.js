9c6332593f23cffe796c87edfc01acfbf702f9fb
aaron <aaronyostiles@gmail.com>
2024-07-09 12:04:56 -0400

    updating styles to adapt to mobile device screens, updating plot to solve resizing issues, general cleanup

--- application/static/js/main.js
+++ application/static/js/main.js
@@ -15,3 +15,88 @@ $(document).ready(function(){
         $('a[href="' + selectedTab + '"]').addClass('active');
     }
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  Prism.highlightAll();
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var route = {
+        x: distances,
+        y: elevations,
+        type: 'scatter',
+        fill: 'tozeroy',  // Shade the area under the curve
+        mode: 'lines',
+        hoverinfo: 'skip',
+    };
+
+    // Create marker for the runner's position
+    var runner_marker = {
+        x: [runner_x],
+        y: [runner_y],
+        mode: 'markers',
+        hoverinfo: 'x+y',
+        hovertemplate: '%{x:.2f} mi, %{y:.0f} ft<extra></extra>',
+        marker: {
+            symbol: 'diamond-dot',
+            size: 14,
+            color: 'purple',
+            line: {
+                color: 'black',
+                width: 1
+            },
+        },
+        name: 'Runner'
+    };
+
+    var data = [route, runner_marker];
+
+    // Define the layout with axis labels
+    var layout = {
+        xaxis: {
+            title: 'Distance (miles)',
+            range: [0, Math.max.apply(null, distances)],
+            fixedrange: true,
+            dtick: 5,
+        },
+        yaxis: {
+            title: 'Elevation (feet)',
+            range: [Math.min.apply(null, elevations), Math.max.apply(null, elevations)],
+            autorange: false,
+            fixedrange: true,
+        },
+        title: 'Elevation Profile',
+        margin: {
+            l: 50,  // left margin
+            r: 50,  // right margin
+            t: 50,  // top margin
+            b: 50   // bottom margin
+        },
+        showlegend: false,
+        hovermode: 'closest',
+        annotations: [
+            {
+                x: runner_x,
+                y: runner_y,
+                xref: 'x',
+                yref: 'y',
+                text: 'Aaron',
+                showarrow: true,
+                arrowhead: 0,
+                ax: 0,
+                ay: -40
+            }
+        ]
+    };
+    var config = {
+      responsive: false,
+      displayModeBar: false, // Hide the modebar
+      scrollZoom: false, // Disable scroll to zoom
+      doubleClick: false, // Disable double click interaction
+      staticPlot: false // Ensure staticPlot is false to allow hover
+    };
+    Plotly.newPlot('profile', data, layout, config); 
+    setInterval(fetchDataAndUpdatePlot, 10000);
+});
