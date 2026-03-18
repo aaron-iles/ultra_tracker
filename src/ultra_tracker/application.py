634425304a0ac35993646788928e226f53ccd65e
aaron <aaronyostiles@gmail.com>
2026-03-17 20:13:36 -0400

    wip

--- src/ultra_tracker/application.py
+++ src/ultra_tracker/application.py
@@ -23,9 +23,7 @@ def create_app() -> tuple:
     app = flask.Flask(__name__)
     app.url_map.strict_slashes = False
     log.info("registering blueprints")
-    app.register_blueprint(api.logs.blueprint, url_prefix=api.logs.URL_PREFIX)
     app.register_blueprint(api.race.blueprint, url_prefix=api.race.URL_PREFIX)
-    app.register_blueprint(api.pings.blueprint, url_prefix=api.pings.URL_PREFIX)
     log.info(f"registering SocketIO handlers")
     socketio.init_app(app)
     return app
