f3c6d143cc84bb91fbfd118a398ed18e81de81f0
aaron <aaron@ultra-tracker.iles>
2026-03-29 15:11:17 -0400

    updating service

--- scripts/deploy_service.sh
+++ scripts/deploy_service.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Make the directory for user-level service.
-mkdir -p /home/${USER}/.config/systemd/user/
-cp ultra-tracker.service /home/${USER}/.config/systemd/user/
+mkdir -p ~/.config/systemd/user/
+cp ultra-tracker.service ~/.config/systemd/user/
 
 # Enable lingering
 sudo loginctl enable-linger ${USER}
