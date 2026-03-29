8c7203fa516ae2c02053063c60257c9ce568b316
aaron <aaronyostiles@gmail.com>
2026-03-29 19:01:13 -0400

    removing in race check

--- src/ultra_tracker/api/race.py
+++ src/ultra_tracker/api/race.py
@@ -14,7 +14,7 @@ URL_PREFIX = "/"
 blueprint = Blueprint("root", __name__)
 
 
-@blueprint.route("/", methods=["POST"])
+@blueprint.route("/ping", methods=["POST"])
 def post_data():
     """
     Receives a ping from the tracker, updates the race object, and logs information.
