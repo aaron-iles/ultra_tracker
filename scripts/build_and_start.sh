39e5ca6327785fd49f18e9f35dead826156ddb3b
aaron <aaron@ultra-tracker.iles>
2026-03-29 15:49:32 -0400

    updating service

--- scripts/build_and_start.sh
+++ scripts/build_and_start.sh
@@ -5,7 +5,7 @@ set -e
 python3 -m build --wheel
 
 if systemctl --user list-unit-files --type=service | grep -q "^ultra-tracker.service"; then
-    systemctl --user start "ultra-tracker"
+    systemctl --user start ultra-tracker
 else
     podman compose up
 fi
